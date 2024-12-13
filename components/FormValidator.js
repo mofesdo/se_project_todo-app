ac37ba6784b09b8079df2e5c2517e6c52be30d9a
Modesto <mofesdo.dev@gmail.com>
2024-12-13 06:03:02 -0800

    Refactor: Form Validation moved to form validator class

---
+++ components/FormValidator.js
@@ -0,0 +1,15 @@
+class FormValidator{
+    constructor(settings, formEl){
+        this._inputSelector = settings.inputSelector;
+        this._submitButtonSelector = settings.submitButtonSelector;
+        this._errorClass = settings.errorClass;
+        this._inputErrorClass = settings.inputErrorClass;
+        this._inactiveButtonClass = settings.inactiveButtonClass;
+        this._formEl = formEl;
+        }
+    enableValidation(){
+
+    }
+}
+
+export default FormValidator;
