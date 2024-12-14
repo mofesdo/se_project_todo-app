d984af52024217fa40ad8b27234bcea72accf16c
Modesto <mofesdo.dev@gmail.com>
2024-12-13 17:39:59 -0800

    Refactor: Enable Validation function moved to formValidator.js

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -7,9 +7,46 @@ class FormValidator{
         this._inactiveButtonClass = settings.inactiveButtonClass;
         this._formEl = formEl;
         }
-    enableValidation(){
 
-    }
+        //TODO implement all other methods
+        
+        __checkInputValidity(inputElement){
+            //TODO implement method (1)
+            // if (!inputElement.validity.valid) {
+            //     showInputError(
+            //       formElement,
+            //       inputElement,
+            //       inputElement.validationMessage,
+            //       settings,
+            //     );
+            //   } else {
+            //     hideInputError(formElement, inputElement, settings);
+            //   }
+        }
+        _setEventListeners(){
+            const inputList = Array.from(
+                this._formEl.querySelectorAll(this._inputSelector),
+              );
+              //TODO - finish implementing seteventlisteners (2)
+            //   const buttonElement = formElement.querySelector(
+            //     settings.submitButtonSelector,
+            //   );
+            
+            //   toggleButtonState(inputList, buttonElement, settings);
+            
+              this._inputList.forEach((inputElement) => {
+                inputElement.addEventListener("input", () => {
+                  this._checkInputValidity();
+                //   toggleButtonState(inputList, buttonElement, settings);
+                });
+              });
+        }
+        enableValidation(){
+            this._formEl.addEventListener("submit", (evt) => {
+            evt.preventDefault();
+            });
+            this._setEventListeners();
+        }
 }
 
 export default FormValidator;
