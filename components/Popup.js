ca3e0fd1ebb94667f3cde36c675ae429523a1c0f
Modesto <mofesdo.dev@gmail.com>
2025-01-08 05:23:03 -0800

    Refactor: Popup class now opens with event listeners

--- components/Popup.js
+++ components/Popup.js
@@ -3,6 +3,9 @@ class Popup{
         this._popupEl = document.querySelector(popupSelector);
         console.log(this._popupEl);
     }
+    open(){
+        this._popupEl.classList.add("popup_visible")
+    }
 
 }
 export default Popup;
--- pages/index.js
+++ pages/index.js
@@ -24,9 +24,9 @@ const section = new Section({
 });
 //Call Section Instance's renderItems method
 
-const openModal = (modal) => {
-  modal.classList.add("popup_visible");
-};
+// const openModal = (modal) => {
+//   modal.classList.add("popup_visible");
+// };
 
 const closeModal = (modal) => {
   modal.classList.remove("popup_visible");
@@ -42,7 +42,7 @@ const generateTodo = (data) => {
 
 
 addTodoButton.addEventListener("click", () => {
-  openModal(addTodoPopupEl);
+  addTodoPopup.open();
 });
 
 addTodoCloseBtn.addEventListener("click", () => {
